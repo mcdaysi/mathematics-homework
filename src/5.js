67092b35b98716fc2f6d9c257e1608eabd398a13
mcdaysi <mc_daysi@gmx.com>
2025-03-07 02:19:37 +0000

    Add 5.js

---
+++ src/5.js
@@ -0,0 +1,10 @@
+// Function to generate a random number between 1 and 100
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+// Calling the function and assigning it to a variable
+const myRandomNumber = getRandomNumber();
+
+// Printing the result
+console.log(myRandomNumber);
